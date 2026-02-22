304bbb2342b35471afeff17d2840919f5d4ed1c7
Roman.Fiser <rosimcxi@gmail.com>
2026-02-22 16:57:30 +0100

    Start

---
+++ Kalkulacka/Kalkulacka.cs
@@ -0,0 +1,28 @@
+﻿namespace Kalkulacka
+{
+    public class Calc
+    {
+        public int Secti(int a, int b)
+        {
+            return a + b;
+        }
+        public int Odectil(int a, int b)
+        {
+            return a - b;
+        }
+
+        public int Nasob(int a, int b)
+        {
+            return a * b;
+        }
+
+        public int Deleni(int a, int b)
+        {            
+            if (b == 0)
+            {
+                throw new DivideByZeroException("Nelze dělit nulou.");
+            }
+            return a / b;
+        }
+    }
+}
---
+++ Kalkulacka/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Kalkulacka
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Calc kalkulacka = new Calc();
+            Console.WriteLine("Soucet :" + kalkulacka.Secti(4, 5));
+            Console.WriteLine("Rozdil :" + kalkulacka.Odectil(4, 5));
+        }
+    }
+}
+
+
